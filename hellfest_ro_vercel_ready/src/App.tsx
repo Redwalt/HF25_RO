0889b9ce4c9e62330100e89319cfaafbf72367cd
Redwalt <ludwig.dumortier@gmail.com>
2025-04-07 09:32:43 +0200

    Add files via upload

---
+++ hellfest_ro_vercel_ready/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>🎸 Hellfest Running Order est prêt !</h1>
+      <p>Cette version fonctionne maintenant avec Vercel ✅</p>
+    </div>
+  );
+}
