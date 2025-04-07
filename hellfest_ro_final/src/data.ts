1bb99ffb936083739885d11d8f1088bcae9e3613
Redwalt <ludwig.dumortier@gmail.com>
2025-04-07 09:15:38 +0200

    Add files via upload

---
+++ hellfest_ro_final/src/data.ts
@@ -0,0 +1,8 @@
+export const data = [
+  { jour: "Jeudi", heure: "16:30 - 17:10", scene: "Mainstage 01", groupe: "Skindred" },
+  { jour: "Jeudi", heure: "18:00 - 18:40", scene: "Valley", groupe: "Slomosa" },
+  { jour: "Vendredi", heure: "23:05 - 00:35", scene: "Mainstage 01", groupe: "Muse" },
+  { jour: "Samedi", heure: "23:10 - 00:40", scene: "Mainstage 01", groupe: "Scorpions" },
+  { jour: "Dimanche", heure: "21:55 - 22:55", scene: "Altar", groupe: "Dethklok" },
+  { jour: "Dimanche", heure: "23:00 - 00:30", scene: "Mainstage 01", groupe: "Linkin Park" }
+];
